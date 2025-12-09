728bf4e7175ca7066f03e0d434fe295630e2fda8
Francisco Rosa <franciscorosa40@gmail.com>
2025-12-09 09:54:25 -0500

    PC 12/9 9:55AM

--- HTML/JavaScript/app.js
+++ HTML/JavaScript/app.js
@@ -1,12 +1,15 @@
-console.log("Hello, World")
+// VARIABLES
 
 let weight = parseInt(prompt("Enter your weight"));
 let height = parseInt(prompt("Enter your height"));
 let gender = prompt("Enter your gender as M or F");
 let bmiCategory;
 
-let firstName = "Francisco";
-let lastName = "Rosa";
-let middleName = "null";
+// BMI CALCULATION
 
-let age = parseInt(prompt("What is your age?"));
+// OUTPUT
+
+/* 
+    Add the output
+    over here 
+*/
